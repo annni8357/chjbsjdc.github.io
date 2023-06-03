99f1e354a99f4cff05885d3fae5e14efb5548cc0
annni8357 <109974883+annni8357@users.noreply.github.com>
2023-06-03 10:10:44 +0530

    dpojio

---
+++ index.js
@@ -0,0 +1,11 @@
+@keyframes scroll-effect {
+    0% {
+      top: 100%; /* Start from bottom */
+    }
+    50% {
+      top: 50%; /* Move to the middle */
+    }
+    100% {
+      top: 0; /* Fully visible at the top */
+    }
+  }
